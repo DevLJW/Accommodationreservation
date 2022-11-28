4bae8ee28da3c0920b7e67de998a46ab6a4bcc20
DevLJW <jaewookv123@gmail.com>
2022-11-28 21:45:28 +0900

    viewpager2 add

--- app/src/main/java/com/example/accommodationreservation/Adapter/HouseViewPagerAdapter.kt
+++ app/src/main/java/com/example/accommodationreservation/Adapter/HouseViewPagerAdapter.kt
@@ -1,4 +1,69 @@
 package com.example.accommodationreservation.Adapter
 
-class HouseViewPagerAdapter {
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.accommodationreservation.HouseModel
+import com.example.accommodationreservation.R
+
+class HouseViewPagerAdapter:ListAdapter<HouseModel,HouseViewPagerAdapter.ItemViewHolder>(differ) {
+
+
+        inner class ItemViewHolder(val view : View): RecyclerView.ViewHolder(view){
+
+            fun bind(houseModel: HouseModel){
+
+               val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
+                val priceTextView = view.findViewById<TextView>(R.id.priceTextView)
+                val thumbnailImageView = view.findViewById<ImageView>(R.id.thumnailImageView)
+
+                titleTextView.text = houseModel.title
+                priceTextView.text = houseModel.price
+                Glide.with(thumbnailImageView.context)
+                    .load(houseModel.imgUrl)
+                    .into(thumbnailImageView)
+
+            }
+
+
+        }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
+            val inflater = LayoutInflater.from(parent.context)
+            return ItemViewHolder(inflater.inflate(R.layout.house_item_viewpager,parent,false))
+
+
+    }
+
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            holder.bind(currentList[position])
+
+    }
+
+    companion object {
+        val differ = object : DiffUtil.ItemCallback<HouseModel>(){
+
+            override fun areItemsTheSame(oldItem: HouseModel, newItem: HouseModel): Boolean {
+                   return oldItem.id == newItem.id
+            }
+
+
+            override fun areContentsTheSame(oldItem: HouseModel, newItem: HouseModel): Boolean {
+               return oldItem == newItem
+            }
+
+
+        }
+    }
+
 }
